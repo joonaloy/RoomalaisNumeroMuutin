3b29849f043544ab7b307b6ad09442af8ee204c0
joona <118251072+joonaloy@users.noreply.github.com>
2023-08-08 10:25:11 +0300

    Update roomalaisnumeromuutin.py

--- roomalaisnumeromuutin.py
+++ roomalaisnumeromuutin.py
@@ -1,7 +1,7 @@
 from itertools import islice
 import collections
 def consume(iterator, n):
-    #pöllitty lol
+    #pöllitty
     #https://stackoverflow.com/questions/17837316/how-do-i-skip-a-few-iterations-in-a-for-loop
 
     "Advance the iterator n-steps ahead. If n is none, consume entirely."
@@ -15,16 +15,17 @@ def consume(iterator, n):
 
 def rm(rl):
     num=0
-
+    #kaikki roomalaiset yhdistelmänumerot ja niiden arabilaiset vastakappaleet
     yhd=["CD","IX","IV","XL","XC","CM",]
     yhdl=[900,9,4,40,90,400]
-
+    #kaikki roomalaiset numerot ja niiden arabilaiset vastakappaleet
     r=["I","V","X","L","C","M","D"]
     l=[1,5,10,50,100,500,1000]
+    #laskee kuinka pitkä numero on
     numbers = iter(range(0,len(rl)))
     oikea=True
 
-    #testi
+    #testaa onko numerossa neljä samaa peräkkäin joka ei ole sallittua
     for t in range(len(rl)):
         if rl[t:t+4]=="IIII" or rl[t:t+4]=="VVVV" or rl[t:t+4]=="XXXX" or rl[t:t+4]=="LLLL" or rl[t:t+4]=="CCCC" or rl[t:t+4]=="MMMM" or rl[t:t+4]=="DDDD":
             print("tämä ei ole sallittu:",rl[t:t+4])
@@ -35,24 +36,25 @@ def rm(rl):
 
         #laskenta
         for i in numbers:
-
-            #print("i on",i)
+            #testaa onko nykyinen index ja seuraava index yhdistelmänumero listassa
             if rl[i:i+2] in yhd:
-                #print("yhd")
+                #jos on, menee yhdistelmänumero listan läpi ja etsii saman jotta voi käyttää indexiä arabi numerolistassa ja lisätä loppunumeroon
                 for y in range(0,len(yhd)):
                     if rl[i:i+2]==yhd[y]:
                         print(yhd[y],"on",yhdl[y])
                         num+=yhdl[y]
+                        #consume skippaa yhen loopin jotta ei laske yhdistelmänumeron toista numeroa itsenään
                         consume(numbers,1)
                     else:
                         continue
-
+            #jos ei ole yhdistelmänumero, testaa onko nykyinen index numero listassa
             elif rl[i] in r:
-                #print("yks")
+                #jos on, menee numero listan läpi ja etsii saman jotta voi käyttää indexiä arabi numerolistassa ja lisätä loppunumeroon
                 for n in range(0,len(r)):
                     if rl[i]==r[n]:
                         print(rl[i],"on",l[n])
                         num+=l[n]
+        #printtaa lopullisen numeron
         print(rl,"lukuna on",num)
     else:
         print("luku on väärin")
